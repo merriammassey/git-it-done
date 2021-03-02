289951616f9b9f6ee78b1167a312da0704a85d9a
Maria M <prov21_1@hotmail.com>
2021-03-02 14:28:15 -0700

    dynamic function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,29 @@
+// test the API
+/*
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      });
+    console.log("outside");
+}
+
+ /*   var response = fetch("https://api.github.com/users/octocat/repos");
+    console.log(response);
   };
+  */
+
+  // get any user's respositories
+ var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
   getUserRepos();
